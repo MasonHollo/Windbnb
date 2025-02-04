9c6663531c43b8343f407111f6bd298ebd56588c
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-02-04 14:59:18 -0800

    mina-updated

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { check } = require('express-validator');
+const { requireAuth } = require('../../utils/auth');
+const { Review } = require('../../db/models');
+
+const router = express.Router();
+
+router.get('/', requireAuth, async (req, res) => {
+  const reviews = await Review.findAll({ where: { userId: req.user.id } });
+  res.json({ Reviews: reviews });
+});
+
+router.post('/:spotId', requireAuth, async (req, res) => {
+  const { spotId } = req.params;
+  const { review, stars } = req.body;
+  
+  const newReview = await Review.create({
+    spotId,
+    userId: req.user.id,
+    review,
+    stars
+  });
+
+  return res.status(201).json(newReview);
+});
+
+module.exports = router;
