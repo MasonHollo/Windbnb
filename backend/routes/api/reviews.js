38e324baa5b9b9edde1695025b51c46cdd380add
masonhollo <masonholloman@gmail.com>
2025-02-05 15:40:51 -0800

    finished delete a spot and most of edit a spot

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -31,11 +31,79 @@ router.get('/current', requireAuth, async (req, res) => {
   res.json({ Reviews: reviews });
 });
 
-//add a image to a review from review Id
+//add a image to a review from review Id ----- still needs work -------
 router.post("/:reviewId/images", async (req, res) => {
   return res.status(200).json({
     message: "this route works"
   })
 })
+
+
+//Delete a review
+router.delete('/:reviewId', requireAuth, async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const { user } = req;
+
+
+    const review = await Spot.findByPk(reviewId);
+
+    if (!review) {
+      return res.status(404).json({
+        message: "review couldn't be found"
+      });
+    }
+
+    if (review.ownerId !== user.id) {
+      return res.status(403).json({
+        message: "Forbidden"
+      });
+    }
+
+
+    await review.destroy();
+
+    return res.status(200).json({
+      message: "Successfully deleted"
+    });
+
+  } catch (e) {
+    return res.status(500).json({
+      message: "Something went wrong",
+      error: error.message
+    });
+  }
+});
+
+// Edit A Review ------ still needs work ------
+router.put('/:reviewId', requireAuth, async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const { user } = req; 
+    const { review ,stars } = req.body;
+
+
+    const reviews = await Review.findByPk(reviewId);
+
+    if (reviews.ownerId !== user.id) {
+      return res.status(403).json({
+        message: "Forbidden"
+      });
+    }
+
+    await reviews.update({
+      review, stars
+    });
+
+
+    return res.status(200).json(reviews);
+
+  } catch (e) {
+    return res.status(500).json({
+      message: "Something went wrong",
+      error: error.message
+    });
+  }
+});
 
 module.exports = router;
