634a447683959188f5c4db0ad9bc0230619d7bb8
.Rashid Jones <artemys@DESKTOP-D1UFVEN.localdomain>
2025-02-07 20:38:49 -0500

    Edit+Delete Bookings, Get All Spots fixed

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require('sequelize');
 
 const { Booking, Spot, User } = require('../../db/models');
 
@@ -66,6 +67,38 @@ router.put('/:bookingId', requireAuth, async (req, res) => {
     return res.status(403).json({ message: "You are not authorized to edit this booking" });
   }
 
+  const newStartDate = new Date(startDate);
+  const newEndDate = new Date(endDate);
+  const today = new Date();
+
+  if (newStartDate < today || newEndDate < today) {
+    return res.status(400).json({ message: "Bookings cannot be made in the past" });
+  }
+
+  if (newEndDate <= newStartDate) {
+    return res.status(400).json({ message: "End date must be after start date" });
+  }
+
+  const existingBooking = await Booking.findOne({
+    where: {
+      spotId: booking.spotId,
+      id: { [Op.ne]: bookingId }, 
+       [Op.or]: [
+        { startDate: { [Op.between]: [newStartDate, newEndDate] } },
+        { endDate: { [Op.between]: [newStartDate, newEndDate] } },
+        { [Op.and]: [
+            { startDate: { [Op.lte]: newStartDate } },
+            { endDate: { [Op.gte]: newEndDate } }
+          ]
+        }
+      ]
+    }
+  });
+
+  if (existingBooking) {
+    return res.status(403).json({ message: "Sorry, this spot is already booked!" });
+  }
+
   await booking.update({ startDate, endDate });
 
   return res.json(booking);
@@ -83,6 +116,12 @@ router.delete('/:bookingId', requireAuth, async (req, res) => {
     return res.status(404).json({ message: "Booking couldn't be found" });
   }
 
+  const today = new Date();
+  const startDate = new Date(booking.startDate);
+
+  if (startDate <= today) {
+    return res.status(403).json({ message: "Bookings that have started cannot be deleted" });
+  }
   if (booking.userId !== req.user.id) {
     return res.status(403).json({ message: "You are not authorized to delete this booking" });
   }
