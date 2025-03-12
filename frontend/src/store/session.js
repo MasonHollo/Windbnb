9c3d46b4c8f6ecf14b00cc40392cd16f81a74e97
masonhollo <masonholloman@gmail.com>
2025-03-12 13:43:38 -0700

    finished phase 3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -58,6 +58,14 @@ export const restoreUser = () => async (dispatch) => {
     dispatch(setUser(data.user));
     return response;
   };
+  
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE'
+    });
+    dispatch(removeUser());
+    return response;
+  };
 //REDUCER
 const initialState = { user: null };
 
