e7976bee552188c3cd8e342312fd953c46890984
masonhollo <masonholloman@gmail.com>
2025-03-16 17:11:27 -0700

    testing for deploy error

--- frontend/src/components/subcomponents/spotcards.jsx
+++ frontend/src/components/subcomponents/spotcards.jsx
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
-function SpotCards({ spot }) {  // Destructuring the 'spot' prop
+function spotcards(spot){
     return (
         <div>
-            <Link to={`/spots/${spot.id}`} key={spot.id} className="spotItem">
-                <h3>{spot.name}</h3>
-                <img id="spotImage" src={spot.previewImage} alt={spot.name} />
-                <p>{spot.city}, {spot.state}, ⭐{spot.avgRating}</p>
-                <p>${spot.price} per night</p>
-            </Link>
+        <Link to={`/spots/${spot.id}`} key={spot.id} className="spotItem">
+            <h3>{spot.name}</h3>
+            <img id='spotImage' src={spot.previewImage} alt={spot.name} />
+            <p>{spot.city}, {spot.state}, ⭐{spot.avgRating}</p>
+            <p>${spot.price} per night</p>
+          
+        </Link>
         </div>
-    );
+    )
 }
 
-export default SpotCards;
+export default spotcards
